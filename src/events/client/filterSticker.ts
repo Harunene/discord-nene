92846edaae0ccc5ade69800c400625cbbca209e7
Harunene <taskinrits@gmail.com>
2024-09-18 16:54:37 +0900

    좀 더 거절 바리에이션을 늘려봄

--- src/events/client/filterSticker.ts
+++ src/events/client/filterSticker.ts
@@ -1,4 +1,4 @@
-import { Emoji, Events, type Message, Sticker } from 'discord.js'
+import { Emoji, Events, type Message, Sticker, StickerResolvable } from 'discord.js'
 import type Bot from '../../structures/Client'
 import Event from '../../structures/Event'
 
@@ -7,18 +7,60 @@ const forbiddenStickerIdList = [
   '1276503786763976724'
 ]
 
+
+type RejectMessageContent = RejectComment | RejectSticker
+type RejectComment = string
+type RejectSticker = {
+  stickers: StickerResolvable[]
+}
+
+const getRandomRejectMessageContent = (message: Message) => {
+  const authorPlaceholder = '%AUTHOR%'
+  const rejectCommentList: RejectComment[] = [
+    `${authorPlaceholder} 💢`,
+    `<a:ui_no:845321450415980556>`,
+    `<:nacho_ded:1247421429041991750>`,
+    `<:mahjong_kuso_game:617986913110065162>`,
+  ].map(comment => comment.replace(authorPlaceholder, `<@${message.author.id}>`)) 
+
+  const rejectStickerList: RejectSticker[] = [
+    "1238848559466680390",
+    "1264805290872672270",
+    "1238849538618429460",
+    "1276514003325292649",
+    "1279379917183913985",
+    "1282974859869290537",
+  ].map(stickerId => ({
+    stickers: [
+      message.guild?.stickers.cache.get(stickerId)
+    ].filter(Boolean) as StickerResolvable[]
+  })) 
+  
+  const rejectMessageContentList: RejectMessageContent[] = [
+    ...rejectCommentList,
+    ...rejectStickerList,
+  ]
+
+  return rejectMessageContentList[Math.floor(Math.random() * rejectMessageContentList.length)]
+}
+
 export default class FilterStickerEvent extends Event {
 
   constructor (client: Bot) {
     super(client, Events.MessageCreate)
   }
 
+  private sendRejectMessage = async (message: Message) => {
+    const rejectMessageContent = getRandomRejectMessageContent(message)
+    await message.channel.send(rejectMessageContent)
+  }
+
   run = async (message: Message): Promise<void> => {
     if (message.stickers.size > 0) {
-      for (const [snowflake, sticker] of message.stickers) {
+      for (const [_, sticker] of message.stickers) {
         if (forbiddenStickerIdList.includes(sticker.id)) {
           await message.delete()
-          await message.channel.send("<a:ui_no:845321450415980556>")
+          await this.sendRejectMessage(message)
         }
       }
     }
