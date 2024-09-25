411dbc1bc233dd379a132c282f69dcb14480523a
Harunene <taskinrits@gmail.com>
2024-09-25 12:51:53 +0900

    금지스티커 기록 채널을 별도 생성

--- src/events/client/filterSticker.ts
+++ src/events/client/filterSticker.ts
@@ -1,52 +1,18 @@
-import { Emoji, Events, type Message, Sticker, StickerResolvable } from 'discord.js'
+import { Events, type Message, TextChannel } from 'discord.js'
 import type Bot from '../../structures/Client'
 import Event from '../../structures/Event'
 
-type RejectMessageContent = RejectComment | RejectSticker
-type RejectComment = string
-type RejectSticker = {
-  stickers: StickerResolvable[]
-}
-
-const getRandomRejectMessageContent = (message: Message) => {
-  const authorPlaceholder = '%AUTHOR%'
-  const rejectCommentList: RejectComment[] = [
-    `${authorPlaceholder} 💢`,
-    `${authorPlaceholder} <a:ui_no:845321450415980556>`,
-    `${authorPlaceholder} <:nacho_ded:1247421429041991750>`,
-    `${authorPlaceholder} <:mahjong_kuso_game:617986913110065162>`,
-  ].map(comment => comment.replace(authorPlaceholder, `<@${message.author.id}>`)) 
-
-  const rejectStickerList: RejectSticker[] = [
-    // "1238848559466680390",
-    // "1264805290872672270",
-    // "1238849538618429460",
-    // "1276514003325292649",
-    // "1279379917183913985",
-    // "1282974859869290537",
-  ].map(stickerId => ({
-    stickers: [
-      message.guild?.stickers.cache.get(stickerId)
-    ].filter(Boolean) as StickerResolvable[]
-  })) 
-  
-  const rejectMessageContentList: RejectMessageContent[] = [
-    ...rejectCommentList,
-    ...rejectStickerList,
-  ]
-
-  return rejectMessageContentList[Math.floor(Math.random() * rejectMessageContentList.length)]
-}
-
 export default class FilterStickerEvent extends Event {
-
+  private notiBotChannel = this.client.channels.cache.get("1288344400925163593") as TextChannel
+  
   constructor (client: Bot) {
     super(client, Events.MessageCreate)
   }
 
   private sendRejectMessage = async (message: Message) => {
-    const rejectMessageContent = getRandomRejectMessageContent(message)
-    await message.channel.send(rejectMessageContent)
+    this.notiBotChannel.send({
+      content: `${message.author.username}, ${message.stickers.map(sticker => sticker.name).join(', ')}`
+    })
   }
 
   run = async (message: Message): Promise<void> => {
